cd279e96c65b7df0be794f3f31510f999535836e
putrafahrezi <putrafahrezi89@gmail.com>
2024-01-04 11:48:06 +0700

    membuat cek login

---
+++ ceklogin_apt.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+if(!isset($_SESSION['sid']) AND !isset($_COOKIE['cid'])){
+  ?>
+  <script>
+    document.location="login_apt.php";
+    </script>
+    <?php
+}
+
+if(isset($_SESSION['sid'])){
+    //variabel element session
+    $l_id       = $_SESSION['sid'];
+    $l_nama     = $_SESSION['snama'];
+    $l_email    = $_SESSION['semail'];
+    
+}elseif(isset($_COOKIE['cid'])){
+    //variabel element cookie
+    $l_id = $_COOKIE['cid'];
+    $l_nama = $_COOKIE['cnama'];
+    $l_email = $_COOKIE['cemail'];
+}
+?>
